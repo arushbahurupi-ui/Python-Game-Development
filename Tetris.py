b2926469bb56f59843c5c7e111f113b6e48fdd25
arushbahurupi-ui <arushbahurupi@gmail.com>
2026-05-27 17:00:34 +0200

    Add files via upload

--- Tetris.py
+++ Tetris.py
@@ -4,7 +4,7 @@ import random
 HEIGHT = 750
 WIDTH = 500
 random_shape = []
-
+topline = None
 x_shape = 200
 y_shape = 0
 index = 0
@@ -37,18 +37,22 @@ shape_rotation = [[[[1],[1],[1],[1]], [[1,1,1,1]]],
 matrix_list = [[0 for i in range(10)] for j in range(15)]
 
 
+colors = ["red", "blue", "green", "yellow", "gray", "brown", "orange", "purple", "pink"]
+
+
 def draw():
     screen.clear()
     matrix_draw()
+
     for i in range(len(random_shape)):
         for j in range(len(random_shape[i])):
             if random_shape[i][j] == 1:
-                screen.draw.filled_rect(Rect(j * 50 + x_shape,i * 50 + y_shape, 50, 50), "red")
+                screen.draw.filled_rect(Rect(j * 50 + x_shape,i * 50 + y_shape, 50, 50), color)
 
 
 def pickshape():
-    global random_shape, x_shape, y_shape, index, rotate_counter
-
+    global random_shape, x_shape, y_shape, index, rotate_counter,color
+    color = random.choice(colors)
     index = random.randint(0,5)
     random_shape = shapes_list[index]
     x_shape = 200
@@ -64,9 +68,9 @@ def rotate():
 
     random_shape = shape_rotation[index][index_1]
 
-def hit_print(xhit, y, shape):
+def hit_print(xhit, yhit, shape):
     x = xhit//50
-    y = y//50
+    y = yhit//50
     for i in shape:
         print(i)
         if i != [0,0,0,0]:
@@ -95,18 +99,57 @@ def matrix_draw():
     for i in range(len(matrix_list)):
         for j in range(len(matrix_list[i])):
             if matrix_list[i][j] == 1:
-                screen.draw.filled_rect(Rect(j*50, i*50,50,50), "red")
+                screen.draw.filled_rect(Rect(j*50, i*50,50,50), "white")
+
+
+def rowsearch():
+    for rowindex in range(len(matrix_list)):
+        if matrix_list[rowindex] != [0,0,0,0,0,0,0,0,0,0]:
+            return rowindex
+    return None
+
+
+def define_hitpositionlocal(x_current):
+    topline = rowsearch()
+    if topline is None:
+        return 750
+
+    columnhitpos = x_current / /50
+
+
+    while topline < len(matrix_list):
+        if matrix_list[topline][columnhitpos] == 1:
+            localhitposition = 750 - ((15 - topline) * 50)
+            return localhitposition
+        topline += 1
+    return 750
+
+def deleteAdd():
+    for row in range(len(matrix_list)):
+        if matrix_list[row] == [1,1,1,1,1,1,1,1,1,1]:
+            print(matrix_list[row])
+            del matrix_list[row]
+            matrix_list.insert(0,[0,0,0,0,0,0,0,0,0,0])
+
+def collision():
+    pass
+
+
+
+
 
 def update():
-    global y_shape, x_shape
+    global y_shape, x_shape, hit_position
+    hit_position = define_hitpositionlocal(x_shape)
+
     if hit_position - y_shape > 50 * len(random_shape):
-        y_shape +=5
+        y_shape +=2
     if hit_position - y_shape == 50 * len(random_shape):
         hit_print(x_shape, y_shape, random_shape)
         x_shape = 0
         y_shape = 0
         pickshape()
-
+    deleteAdd()
 
 
 
